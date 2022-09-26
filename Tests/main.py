13ad771b18aad3564fd6c231d41e1f3661d53152
Lesticker <dawid.kacprzyk1@gmail.com>
2022-09-26 23:43:24 +0200

    renamed file. deleted unnecessary imports, fixed whitespaces and added posibility to choose by user how many photos he wants to like

--- Tests/main.py
+++ Tests/main.py
@@ -1,19 +1,14 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "."))
+from Pages.login_page import LoginPage
+from Pages.images_page import ImagesSite
+from Pages.main_page import Application
+from Pages.settings import settings_data
 import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.support.ui import Select
-# from selenium.webdriver.common.alert import Alert
-import time
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__),"..","."))
-import time
-from Pages.loginpage import LoginPage
-from Pages.imagessite import ImagesSite
-from Pages.app import Application
+
 
 class BuyTest(unittest.TestCase):
     
@@ -39,7 +34,7 @@ class BuyTest(unittest.TestCase):
         liking = ImagesSite(driver)
         # go into first img from DOM
         liking.get_into_img()
-        for i in range(10):
+        for i in range(settings_data.entered_number_of_likes):
             liking.like_photo()
 
 
@@ -48,13 +43,7 @@ class BuyTest(unittest.TestCase):
         cls.driver.close()
         cls.driver.quit()
         print("Test Completed")
-    
-if __name__ =='__main__':
+
+
+if __name__ == '__main__':
     unittest.main()
-
-
-#options = webdriver.ChromeOptions()
-
-
-
-
