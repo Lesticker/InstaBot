aec43632aafff97537c9544611dd8ce58d0800e7
Lesticker <dawid.kacprzyk1@gmail.com>
2022-10-04 22:40:26 +0200

    added randomness and another DOM element to click

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from Pages.locators import Locators
 from Pages.settings import settings_data
 import time
+import random
 
 
 class LoginPage():
@@ -12,12 +13,12 @@ class LoginPage():
 
     def login_to_account(self):
         WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((Locators.cookie_xpath))).click()
-        time.sleep(2)
+        time.sleep(random.uniform(3, 4))
         input1 = WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((Locators.username_xpath)))
         input1.send_keys(settings_data.entered_login)
         input2 = WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((Locators.password_xpath)))
         input2.send_keys(settings_data.entered_password)
+        time.sleep(random.uniform(0.5, 2))
         self.driver.find_element(*Locators.login_button).click()
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((Locators.disable_notification))).click()
         WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((Locators.not_now))).click()
-        # currently not used in case of pop up returns use below command
-        # WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((Locators.disable_notification))).click()
