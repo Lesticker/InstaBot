81569dc85a974ec928eaca34b69984a88fb7cbe1
Lesticker <dawid.kacprzyk1@gmail.com>
2022-10-04 22:39:58 +0200

    changed disable_notification locator xpath

--- Pages/locators.py
+++ Pages/locators.py
@@ -9,7 +9,7 @@ class Locators():
     login_button = (By.XPATH, "/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div")
     cookie2_xpath = (By.XPATH, "/html/body/div[7]/div/div/div/div[1]/div/div[3]/div[3]/div/div[3]/div[1]/div[1]/div[2]/div[1]/span")
     not_now = (By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]")
-    disable_notification = (By.XPATH, "/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[3]/button[2]")
+    disable_notification = (By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div/div/button")
     search_popup = (By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[3]/div/div[3]/div/button/div/div/div/div/div[2]/div")
     search_input = (By.XPATH, "//input[@placeholder='Szukaj']")
     first_img_xpath = (By.XPATH, "(//div[@class='_aabd _aa8k _aanf'])[1]")
